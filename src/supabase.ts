ac340a40b8210cebdccfdc8969fd9041d775c386
crfuentes <crfuentes@gmail.com>
2026-06-01 16:17:09 -0400

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://auennuzkgpfexxpxkvrh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1ZW5udXprZ3BmZXh4cHhrdnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNjIxNDYsImV4cCI6MjA5MzYzODE0Nn0.g9swStHK_O610WMNFJDFPOLdoToxo14C-7jWpQcr2GM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
